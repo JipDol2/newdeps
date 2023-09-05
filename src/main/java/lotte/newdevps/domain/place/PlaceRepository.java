781e694455a76dddceed6c8c1ca8457d32593e2c
leeseongju <leeseongju@lotte.net>
2023-09-05 10:34:43 +0900

    fix() : mysql 에는 table 대소문자를 구분

--- src/main/java/lotte/newdevps/domain/place/PlaceRepository.java
+++ src/main/java/lotte/newdevps/domain/place/PlaceRepository.java
@@ -11,6 +11,6 @@ import java.util.List;
 public interface PlaceRepository extends JpaRepository<Place,Long> {
 
     @Query(value = "SELECT p.id,p.latitude,p.longitude,p.category,p.placeName,p.address,p.categoryName,p.starRating,p.detailUrl,p.imageUrl,CASE WHEN b.id IS NOT NULL THEN 'Y' ELSE 'N' END AS BookmarkStatus" +
-            " FROM Place p LEFT JOIN (SELECT b.id,b.place_id FROM Bookmark b WHERE b.user_id = :loginId)B ON p.id = b.place_id",nativeQuery = true)
+            " FROM PLACE p LEFT JOIN (SELECT b.id,b.place_id FROM BOOKMARK b WHERE b.user_id = :loginId)b ON p.id = b.place_id",nativeQuery = true)
     List<PlaceDTOInterface> findByAllPlaceDTO(@Param("loginId")Long loginId);
 }
